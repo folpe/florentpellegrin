eb7a0694dd4a7b6e78e46c80e4f04d1816e0640d
Florent Pellegrin <florent.pellegrin@gmail.com>
2023-10-18 14:28:19 +0200

    test5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Main } from './pages/Main'
 import { Wip } from './pages/Wip'
 
-function App() {
-  const isWip = import.meta.env.VITE_WIP_FLAG === 'true'
+const isWip = import.meta.env.VITE_WIP_FLAG === 'true'
 
-  console.log(isWip)
-  console.log('env : ', import.meta.env)
+console.log(isWip)
+console.log('env : ', import.meta.env)
+function App() {
   if (isWip) {
     return <Wip />
   }
