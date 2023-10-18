e21d9d2ae5c0dde222c27da52125e033dd0d3c8d
Florent Pellegrin <florent.pellegrin@gmail.com>
2023-10-18 09:22:39 +0200

    feat(main): restructure main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { common } from './data/config'
+import { Main } from './pages/Main'
 import { Wip } from './pages/Wip'
 
 function App() {
@@ -9,9 +10,9 @@ function App() {
   }
 
   return (
-    <>
-      New Site for Florent PELLEGRIN
-    </>
+    <Main>
+      New Site for Florent PELLEGRIN the best
+    </Main>
   )
 }
 
---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface MainProps {
+  children: React.ReactNode
+}
+
+const Main: React.FC<MainProps> = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export  {Main}
