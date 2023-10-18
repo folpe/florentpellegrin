6f3302245ffc8137ef708d2361d88daa29e79f3c
Florent Pellegrin <florent.pellegrin@gmail.com>
2023-10-18 09:24:32 +0200

    feat(navigation): add navigation component

---
+++ src/components/molecules/Navigation/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navigation: React.FC = () => {
+  return (
+    <ul>
+      <li>About</li>
+      <li>Experience</li>
+      <li>Work</li>
+      <li>Contact</li>
+    </ul>
+  )
+}
+
+export {Navigation}
